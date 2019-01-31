7d7944b216528c0b276d92b4245148ab3967027d
Hans Tremmel <hans.tremmel@gmail.com>
2019-01-30 20:46:12 -0800

    With a server that doesn't work.

---
+++ src/main/Outcast.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.StdIn;
+
+public class Outcast {
+    public Outcast(WordNet wordnet) {
+        // contructor takes a WordNet object.
+    }
+
+    public String outcast(String[] nouns) {
+        // given an array of WordNet nounts, return an outcast.
+    }
+
+    public static void main(String[] nounts) {
+
+    }
+
+}
